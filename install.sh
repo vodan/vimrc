70879361395e3ea576c695996acdfad1a9a8589f
Fabian Panzer <vodan@users.noreply.github.com>
2016-11-17 08:08:35 +0100

    Update install process
    now the install process will update the links if they exists.

--- install.sh
+++ install.sh
@@ -11,6 +11,17 @@ if [ ! -d "$HOME/.vim/backup" ]; then
     mkdir -p $HOME/.vim/backup
 fi
 
+# unlink existing links to update them.
+if [ -L "$HOME/.vimrc" ]; then
+    unlink $HOME/.vimrc 
+fi
+if [ -L "$HOME/.vim/plugin.config" ]; then
+    unlink $HOME/.vim/plugin.config 
+fi
+if [ -L "$HOME/.vim/privatSnips" ]; then
+    unlink $HOME/.vim/privatSnips 
+fi
+
 SCRIPT=$(dirname $(readlink -e $0))
 ln -s $SCRIPT/vimrc $HOME/.vimrc
 ln -s $SCRIPT/plugin.config $HOME/.vim/plugin.config
